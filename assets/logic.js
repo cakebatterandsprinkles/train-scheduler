586953781cff6b5b2e41c05d4e17b8b9df1c5b7d
yagmurcetintas <yagmurcetin@gmail.com>
2019-06-12 16:31:57 -0400

    added HTML structure and basic styling :art:

---
+++ assets/logic.js
@@ -0,0 +1,15 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCF4t3oMUIWvt_bDz7i9ppyqTBz0ka_USQ",
+    authDomain: "first-awesome-project-dda00.firebaseapp.com",
+    databaseURL: "https://first-awesome-project-dda00.firebaseio.com",
+    projectId: "first-awesome-project-dda00",
+    storageBucket: "first-awesome-project-dda00.appspot.com",
+    messagingSenderId: "753958578625",
+    appId: "1:753958578625:web:560395e2115649cd"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
+
+  
